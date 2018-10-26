95b8ab49c9801b623851c7805c88c8384c4499f3
Elisa <elisacho81@gmail.com>
2018-10-26 09:20:19 +0900

    onClick and onLongClick buttons

--- app/src/main/java/com/jmj/edu/event/MainActivity.java
+++ app/src/main/java/com/jmj/edu/event/MainActivity.java
@@ -7,10 +7,12 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, View.OnLongClickListener {
 
-        Button button;
-        Button button1;
+    Button button;
+    Button button1;
+    Button button2;
+    Button button3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,19 +24,60 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         button1 = (Button) findViewById(R.id.button1);
         button1.setOnClickListener(this);
+
+        button2 = (Button) findViewById(R.id.button2);
+        button2.setOnLongClickListener(this);
+
+        button3 = (Button) findViewById(R.id.button3);
+        button3.setOnLongClickListener(this);
+
     }
 
     @Override
     public void onClick(View v) {
-        String text = ((Button)v).getText().toString();
+        String text = ((Button) v).getText().toString();
         TextView textView = (TextView) findViewById(R.id.textView);
+        int id = v.getId();
 
-        if(v.getId() == R.id.button) {
-            Toast.makeText(this, ((Button)v).getText(), Toast.LENGTH_SHORT ).show();
-            textView.setText(text.toString());
-        } else {
-            Toast.makeText(this, ((Button)v).getText(), Toast.LENGTH_SHORT ).show();
-            textView.setText(text.toString());
+        switch (id) {
+            case R.id.button:
+                Toast.makeText(this, ((Button) v).getText(), Toast.LENGTH_SHORT).show();
+                textView.setText(text.toString());
+                break;
+            case R.id.button1:
+                Toast.makeText(this, ((Button) v).getText(), Toast.LENGTH_SHORT).show();
+                textView.setText(text.toString());
+                break;
+            case R.id.button2:
+                Toast.makeText(this, ((Button) v).getText(), Toast.LENGTH_SHORT).show();
+                textView.setText(text.toString());
+                break;
+            case R.id.button3:
+                Toast.makeText(this, ((Button) v).getText(), Toast.LENGTH_SHORT).show();
+                textView.setText(text.toString());
+                break;
+
         }
+
+    }
+
+    @Override
+    public boolean onLongClick(View v) {
+        String text = ((Button) v).getText().toString();
+        TextView textView = (TextView) findViewById(R.id.textView);
+        int id = v.getId();
+
+
+        switch (id) {
+            case R.id.button2:
+                Toast.makeText(this, ((Button) v).getText(), Toast.LENGTH_SHORT).show();
+                textView.setText(text.toString());
+                break;
+            case R.id.button3:
+                Toast.makeText(this, ((Button) v).getText(), Toast.LENGTH_SHORT).show();
+                textView.setText(text.toString());
+                break;
+        }
+        return false;
     }
 }
